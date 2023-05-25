5fcca265e7c2a66f4c56c1515392b0ec1e68ef63
Frédéric Jouault <f.jouault@gmail.com>
2023-05-25 16:36:50 +0200

    updated AnimUML

--- prose/conception/animUML/conceptionGenerale/Config.mk
+++ prose/conception/animUML/conceptionGenerale/Config.mk
@@ -3,6 +3,6 @@ MODEL_PATH=.
 MODEL=conceptionGenerale
 MODEL_EXT=html
 FOLDED_STATES=
-PARAMS=
+PARAMS=hideActorClasses=true hideOperationsAlreadyOnConnectors=true
 ACTIVITIES=#class.Speaker.showScreen pour afficher le diagramme d'activité de la classe Speaker pour la méthode showScreen
 MANU_PUML=
